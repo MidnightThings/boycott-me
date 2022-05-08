b75d048bc1a3d8b3606ba5a47898f3e6d48b0753
FP <felix.petrasek@googlemail.com>
2022-05-08 18:00:37 +0200

    init

---
+++ vue-templates/Index.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+       <p>{{ testFnc() }}</p>
+   </div>
+</template>
+<script>
+    export default {
+        name: "idx",
+        props: {test: {type: String}},
+        data(props) {
+            function testFnc(){
+                return "hello";
+            }
+
+           return {testFnc}
+        }
+    }
+</script>
+<style scoped>
+
+</style>
